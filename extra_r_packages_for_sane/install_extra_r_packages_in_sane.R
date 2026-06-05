6cd9a03d87723479da7913d675e5036ff610cbeb
Qixiang Fang <fqixiang@gmail.com>
2026-06-05 19:14:17 +0200

    Use explicit pkg_* paths and install dsl from dir
    Rename package_main_folder_path to pkg_main_folder_path and add pkg_cran_folder_path and pkg_source_folder_path for clearer folder handling. Use pkg_cran_folder_path to install CRAN binary packages and install the dsl package from its unzipped source directory (dsl-master) instead of a zipped file; update related comments accordingly.

--- extra_r_packages_for_sane/install_extra_r_packages_in_sane.R
+++ extra_r_packages_for_sane/install_extra_r_packages_in_sane.R
@@ -1,11 +1,13 @@
 # The following code installs extra R packages for SICSS 2026.
 # It is meant to be run within the SANE environment.
 
-package_main_folder_path <- "S:/scripts/extra_r_packages_for_sane/downloaded_r_packages"
+pkg_main_folder_path <- "S:/scripts/extra_r_packages_for_sane/downloaded_r_packages"
+pkg_cran_folder_path <- file.path(pkg_main_folder_path, "cran")
+pkg_source_folder_path <- file.path(pkg_main_folder_path, "source")
 
 # Install the CRAN binary packages first
 files <- list.files(
-  file.path(package_main_folder_path, "cran"),
+  pkg_cran_folder_path,
   pattern = "\\.zip$",
   full.names = TRUE
 )
@@ -16,14 +18,11 @@ install.packages(
   type = "win.binary"
 )
 
-# Install the `dsl` package, which is only available as a source package
-dsl_package_path <- file.path(
-  file.path(package_main_folder_path, "source"),
-  "dsl.zip"
-)
+# Install the `dsl` package from its unzipped folder, which is only available as a source package
+dsl_source_dir <- file.path(pkg_source_folder_path, "dsl-master")
 
 install.packages(
-  dsl_package_path,
+  dsl_source_dir,
   repos = NULL,
   type = "source"
 )
