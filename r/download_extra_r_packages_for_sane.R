c9ad4db44a9b48ad28962c39612e39dfdd19e1e6
Qixiang Fang <fqixiang@gmail.com>
2026-06-05 16:25:55 +0200

    Use here() for package dir and add dsl download
    Create package download directory under r/ using here::here and update destdir to use that path. Add a comment and download the GitHub ZIP for the `dsl` package into the same directory. Keeps CRAN package download behavior (win.binary) unchanged.

--- r/download_extra_r_packages_for_sane.R
+++ r/download_extra_r_packages_for_sane.R
@@ -1,12 +1,14 @@
 # The following code downloads the extra R packages for SICSS 2026.
 # It is meant to be run on your local machine with Internet access.
 
+# Download CRAN packages and their dependencies
 pkgs <- c("grf", "estimatr", "SuperLearner", "arm", "matrixcalc",
           "sf", "terra", "tidyterra", "lwgeom", "tmap", "leaflet",
           "mapview", "spdep", "spatialreg", "sfdep")
 
 dir_name <- "extra_r_packages_for_sane"
-dir.create(dir_name, showWarnings = FALSE)
+dir_path <- here::here("r", dir_name)
+dir.create(dir_path, showWarnings = FALSE)
 
 ap <- available.packages(
   repos = "https://cloud.r-project.org",
@@ -25,7 +27,14 @@ all_pkgs <- setdiff(all_pkgs, rownames(installed.packages(priority = "base")))
 
 download.packages(
   all_pkgs,
-  destdir = dir_name,
+  destdir = dir_path,
   repos = "https://cloud.r-project.org",
   type = "win.binary"
+)
+
+# Download the `dsl` package from GitHub
+download.file(
+  "https://github.com/naoki-egami/dsl/archive/refs/heads/master.zip",
+  destfile = file.path(dir_path, "dsl.zip"),
+  mode = "wb"
 )
